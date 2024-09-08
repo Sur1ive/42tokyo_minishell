c4ecc5f3a5fa74bcb531706e110a58e2466c3c7a
yxu <penguinxx2@gmail.com>
2024-09-08 15:17:05 +0900

    refactor exec functions

---
+++ srcs/builtins/exec_builtin.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exec_builtin.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yxu <yxu@student.42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/08 13:53:13 by yxu               #+#    #+#             */
+/*   Updated: 2024/09/08 13:53:56 by yxu              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int	is_builtin(char *command)
+{
+	if (!(ft_strcmp(command, "echo") && ft_strcmp(command, "pwd")
+			&& ft_strcmp(command, "env") && ft_strcmp(command, "cd")
+			&& ft_strcmp(command, "export") && ft_strcmp(command, "unset")
+			&& ft_strcmp(command, "exit")))
+		return (1);
+	return (0);
+}
+
+int	exec_bulitin(char **args, char ***envpp)
+{
+	char	*command;
+
+	command = args[0];
+	if (ft_strcmp(command, "echo") == 0)
+		return (echo(args));
+	if (ft_strcmp(command, "pwd") == 0)
+		return (pwd());
+	if (ft_strcmp(command, "env") == 0)
+		return (env(*envpp));
+	if (ft_strcmp(command, "cd") == 0)
+		return (cd(args, envpp));
+	if (ft_strcmp(command, "export") == 0)
+		return (export(args, envpp));
+	if (ft_strcmp(command, "unset") == 0)
+		return (unset(args, envpp));
+	if (ft_strcmp(command, "exit") == 0)
+		builtin_exit(*envpp);
+	return (-1);
+}
--- srcs/main.c
+++ srcs/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nakagawashinta <nakagawashinta@student.    +#+  +:+       +#+        */
+/*   By: yxu <yxu@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/28 14:51:09 by yxu               #+#    #+#             */
-/*   Updated: 2024/09/02 04:50:36 by nakagawashi      ###   ########.fr       */
+/*   Updated: 2024/09/08 15:04:28 by yxu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int	main(int argc, char **argv)
 	set_signal();
 	if (argc > 1)
 	{
-		exec_file(&argv[1], &envp);
+		exec_file(argv[1], &argv[1], envp);
 		free2(envp);
 		return (0);
 	}
@@ -31,7 +31,8 @@ int	main(int argc, char **argv)
 	{
 		free(input);
 		add_history(rl_line_buffer);
-		args = parseline(rl_line_buffer, envp);
+		// args = parseline(rl_line_buffer, envp);
+		args = ft_split(rl_line_buffer, ' ');
 		exec(args, &envp);
 		free2(args);
 		input = readline("$ ");
